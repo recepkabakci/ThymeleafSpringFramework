4a011e30d82384a96a6ae25522315f6cbb9a77de
RECEP KABAKCI <recepkabakci90@gmail.com>
2023-04-22 19:36:13 +0200

    Jsf bean,index.html

---
+++ src/main/java/com/recepkabakci/ThymeleafSpringFramework/ThymeleafSpringFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.recepkabakci.ThymeleafSpringFramework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafSpringFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafSpringFrameworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/recepkabakci/ThymeleafSpringFramework/ThymeleafSpringFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recepkabakci.ThymeleafSpringFramework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafSpringFrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
