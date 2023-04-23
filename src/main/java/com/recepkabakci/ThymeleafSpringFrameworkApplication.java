1bcf0ce6b624adb221343a03a3e91211135bb5c8
RECEP KABAKCI <recepkabakci90@gmail.com>
2023-04-23 14:48:22 +0200

    main app

---
+++ src/main/java/com/recepkabakci/ThymeleafSpringFrameworkApplication.java
@@ -0,0 +1,12 @@
+package com.recepkabakci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafSpringFrameworkApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleafSpringFrameworkApplication.class,args);
+
+    }
+}
