71cfc7b28a853023f930cddc5b13463b7ba5c1df
RECEP KABAKCI <recepkabakci90@gmail.com>
2023-04-23 14:24:41 +0200

    bean dto

--- src/main/java/com/recepkabakci/dto/BeanDto.java
+++ src/main/java/com/recepkabakci/dto/BeanDto.java
@@ -1,5 +1,16 @@
 package com.recepkabakci.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.log4j.Log4j;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Builder
 public class BeanDto {
     private Long beanId;
     private String beanName;
