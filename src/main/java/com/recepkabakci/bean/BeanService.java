6c747287fc1c306d5bb8177710ca21e674899f38
RECEP KABAKCI <recepkabakci90@gmail.com>
2023-04-23 14:47:18 +0200

    bean service

--- src/main/java/com/recepkabakci/bean/BeanService.java
+++ src/main/java/com/recepkabakci/bean/BeanService.java
@@ -3,11 +3,13 @@ package com.recepkabakci.bean;
 import com.recepkabakci.dto.BeanDto;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class BeanService {
 
-    @Bean
+    @Bean(initMethod = "initialBeanMethod",destroyMethod = "destroyBeanMethod")
+    @Scope("singleton")
     public BeanDto beanDto(){
         return BeanDto.builder()
                 .beanId(0L).beanName("beanName1").beanData("beanData1")
