3ce53db934c9f3e6916b68d68155496745b53bdf
RECEP KABAKCI <recepkabakci90@gmail.com>
2023-04-23 14:25:13 +0200

    b

---
+++ src/main/java/com/recepkabakci/bean/BeanService.java
@@ -0,0 +1,16 @@
+package com.recepkabakci.bean;
+
+import com.recepkabakci.dto.BeanDto;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BeanService {
+
+    @Bean
+    public BeanDto beanDto(){
+        return BeanDto.builder()
+                .beanId(0L).beanName("beanName1").beanData("beanData1")
+                .build();
+    }
+}
