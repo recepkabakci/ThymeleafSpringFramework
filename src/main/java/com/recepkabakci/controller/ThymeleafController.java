1c211a19e3e2a0768f7bb74e7c0d131b08b5b798
RECEP KABAKCI <recepkabakci90@gmail.com>
2023-04-25 16:20:41 +0200

    added new methods

--- src/main/java/com/recepkabakci/controller/ThymeleafController.java
+++ src/main/java/com/recepkabakci/controller/ThymeleafController.java
@@ -2,6 +2,7 @@ package com.recepkabakci.controller;
 
 import lombok.Getter;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -14,4 +15,21 @@ public class ThymeleafController {
     public String thymeleaf1(){
         return "thymeleaf1";
     }
+    //http://localhost:8080/thymeleaf2
+    @GetMapping("/thymeleaf2")
+    public String thymeleaf2(Model model){
+        model.addAttribute("key_model","I am Model.");
+        model.addAttribute("key_model1","I am Model 1 and ready.");
+        return "thymeleaf2";
+    }
+
+    //http://localhost:8080/thymeleaf3
+    @GetMapping("/thymeleaf3")
+    public String thymeleaf3(Model model){
+        model.addAttribute("key_model","I am Model.");
+        model.addAttribute("key_model1","I am Model 1 and ready.");
+        return "thymeleaf_file/thymeleaf3";
+    }
+
+
 }
